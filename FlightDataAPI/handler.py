67529e3e39a5ac2d001e18f1fc41681eac48a5ca
Scott Nelson <scott.nelson@onepeloton>
2022-11-20 21:03:06 -0500

    Setup Datadog integration including logs; Refactor main handler, clean up loging a bit

--- FlightDataAPI/handler.py
+++ FlightDataAPI/handler.py
@@ -1,22 +1,28 @@
 import json
 import boto3
+import logging
 
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+'''
+Returns JSON in following format:
+{
+    "Airline Name": {"ontime_percentage": float, "cancelled_percentage": float},
+    ...
+}
+'''
 def hello(event, context):
-    body = {
-        "message": "Go Serverless v3.0! Your function executed successfully!",
-        "input": event,
-    }
-
+    # Note: Single line logs are ingested into datadog fine, but json dumps not so much
+    # Or, as shown below, don't need to json.dumps at all?
+    # Maybe need to tweak log formatting here, or parsing in datadog
+    logger.info("Lambda handler starting; Using built-in logger")
     BUCKET_NAME = 'airlinereliabilitystats'
     key = 'reliabilitymetrics.json'
-    s3 = boto3.resource('s3')
-    obj = s3.Object(BUCKET_NAME, key)
+
+    obj = boto3.resource('s3').Object(BUCKET_NAME, key)
     fetched_data = obj.get()['Body'].read().decode('utf-8')
-    print('did it work?, fetched data:')
-    print(fetched_data)
-    # file = boto3.resource('s3').Bucket(BUCKET_NAME).download_file(
-    #     'jetblue', '/tmp/airline_reliability_stats.csv')
-    # print(file)
+    logger.info("fetched_data: " + fetched_data)
 
     response = {"statusCode": 200, "body": fetched_data}
 
